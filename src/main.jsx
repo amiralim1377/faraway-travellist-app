e093d5d236483db07c195c71aec306bed11bf11b
amirali <alimoradi7718@gmail.com>
2024-10-31 21:58:37 +0330

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-createRoot(document.getElementById("root")).render(<App />);
+import store from "./store.js";
+import { PersistGate } from "redux-persist/es/integration/react";
+import { persistStore } from "redux-persist";
+const persistor = persistStore(store);
+
+createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <App />
+  </PersistGate>
+);
--- src/store.js
+++ src/store.js
@@ -1,9 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rememerReducer from "./reducer/reducer";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfing = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  remember: rememerReducer,
+});
+
+const persistedReducer = persistReducer(persistConfing, reducer);
+
 const store = configureStore({
-  reducer: {
-    remember: rememerReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
